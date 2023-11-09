a16991c4c85a57860b0c92e4adf7ca2440411d9f
NotSalt13 <andreyfrolovo2016@gmail.com>
2023-11-09 09:53:42 +0300

    Update JavaSript.js

--- JS/JavaSript.js
+++ JS/JavaSript.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
     dots: true,
     slidesToShow: 3,
     centerMode: true,
-    autoplay: false,
+    autoplay: true,
     responsive: [
       {
         breakpoint: 1240,
